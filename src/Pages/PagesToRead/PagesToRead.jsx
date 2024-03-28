91912644b4a2eaf6bcd13c09b26149d33ba16689
saidul24 <jihadh32978@gmail.com>
2024-03-28 23:34:07 +0600

    added bar chart

--- src/Pages/PagesToRead/PagesToRead.jsx
+++ src/Pages/PagesToRead/PagesToRead.jsx
@@ -1,8 +1,40 @@
+import { useEffect, useState } from "react";
+import { Bar, BarChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import { getStoredBookRead } from "../../Utility/LocalStorage";
 
 const PagesToRead = () => {
+    const [barData, setBarData] = useState();
+    const storedReadBook = getStoredBookRead()
+
+    useEffect(() => {
+        fetch('https://saidul24.github.io/books-json-data/books.json')
+            .then(res => res.json())
+            .then(data => {
+                if (data.length > 0) {
+                    const bookReads = [];
+                    for (const id of storedReadBook) {
+                        const book = data.find(book => book.bookId === id)
+                        if (book) {
+                            bookReads.push(book)
+                        }
+                    }
+                    setBarData(bookReads)
+                }
+            });
+    }, [])
+
+
+
     return (
         <div>
-            pages to read
+            <ResponsiveContainer width="50%" aspect={3}>
+                <BarChart data={barData} width={400} height={400}>
+                    <XAxis dataKey="bookName"/>
+                    <YAxis/>
+                    <Tooltip></Tooltip>
+                    <Bar dataKey="totalPages" fill="#8883d8"/>
+                </BarChart>
+            </ResponsiveContainer>
         </div>
     );
 };
